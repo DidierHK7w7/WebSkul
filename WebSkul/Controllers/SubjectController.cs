ed95f8b6c937c3403a8cef259f174306a2944dfd
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-16 20:19:10 -0500

    Lección 14. Vistas parciales

--- WebSkul/Controllers/SubjectController.cs
+++ WebSkul/Controllers/SubjectController.cs
@@ -8,11 +8,22 @@ namespace WebSkul.Controllers
         public IActionResult Index()
         {
             var subject = new Subject { UniqueId = Guid.NewGuid().ToString(), Name = "Video Games" };
-
             ViewBag.DynamicThing = "Hello world xd";
             ViewBag.Date = DateTime.Now;
-
             return View(subject);
+        }
+
+        public IActionResult MultiSubject()
+        {
+            var listSubjects = new List<Subject>(){
+                    new Subject{Name = "Math", UniqueId = Guid.NewGuid().ToString()},
+                    new Subject{Name = "English", UniqueId = Guid.NewGuid().ToString()},
+                    new Subject{Name = "Videogames", UniqueId = Guid.NewGuid().ToString()},
+                    new Subject{Name = "Music", UniqueId = Guid.NewGuid().ToString()},
+                    new Subject{Name = "Web apps development", UniqueId = Guid.NewGuid().ToString()}
+             };
+
+            return View(listSubjects);      //Indicamos la vista como parametro
         }
     }
 }
