243c34a48092b828eb92ee348fc5e5f714d70aef
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-17 00:46:17 -0500

    Lección 15. Mostrando otras listas de objetos

---
+++ WebSkul/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSkul.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebSkul.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var student = new Student { Name = "Ran Mitake", UniqueId = Guid.NewGuid().ToString() };
+            
+            return View(student);
+        }
+
+        public IActionResult MultiStudent()
+        {
+            var studentList = RandomStudentsGenerator();
+
+            return View(studentList);      //Indicamos la vista como parametro
+        }
+
+        private List<Student> RandomStudentsGenerator()
+        {
+            string[] firstName = { "Carl", "Big", "Cesar", "OG", "Wu", "Mike", "Madd" };
+            string[] middleName = { "Mitake", "Hikawa", "Yamabuki", "Ichigaya", "Aoba", "Hitori", "Lawrence" };
+            string[] lastName = { "Johnson", "Smoke", "Vialpando", "Loc", "Zi Mu", "Toreno", "Dogg" };
+            //Producto cartesiano con LinQ
+            var studentList = from n1 in firstName
+                              from n2 in middleName
+                              from a1 in lastName
+                              select new Student { Name = $"{n1} {n2} {a1}" };
+            return studentList.OrderBy((stn) => stn.UniqueId).ToList();     //El delegado Obrder By ordena por el ide unico y Take trunca la lista de n alumnos a un numero especifico
+        }
+    }
+}
