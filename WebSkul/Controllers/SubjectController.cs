7ab9401a5a597569f71e777533229650725d5ea9
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-18 17:40:15 -0500

    Lección 24. Ajuste de enrutamiento

--- WebSkul/Controllers/SubjectController.cs
+++ WebSkul/Controllers/SubjectController.cs
@@ -8,11 +8,25 @@ namespace WebSkul.Controllers
     public class SubjectController : Controller
     {
         private SchoolContext _context;
-        public IActionResult Index()
+
+        [Route("Subject/Index/")]
+        [Route("Subject/Index/{subjectId}")]
+
+        public IActionResult Index(string subjectId)
         {
-            ViewBag.DynamicThing = "Hello world xd";
-            ViewBag.Date = DateTime.Now;
-            return View(_context.Subjects.FirstOrDefault());  //Trae la primera o la asignatura por defecto de la db
+            if (!string.IsNullOrWhiteSpace(subjectId))       //Si subjectId no es nulo
+            {
+                ViewBag.DynamicThing = "Hello world xd";
+                ViewBag.Date = DateTime.Now;
+                var subject = from sub in _context.Subjects
+                              where sub.Id == subjectId     //trae la asignatura que el id coincida con el id que pasemos
+                              select sub;
+                return View(subject.SingleOrDefault());  //Trae la primera o la asignatura por defecto de la db
+            }
+            else
+            {
+                return View("MultiSubject",_context.Subjects);  //Si subjectId es nulo, redirecciona a todas las asignaturas
+            }
         }
 
         public IActionResult MultiSubject()
