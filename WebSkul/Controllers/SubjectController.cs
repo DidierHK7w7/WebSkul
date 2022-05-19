6feea4c1a0e3e7e1070cd549867937c84b509b2d
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-19 18:07:02 -0500

    Edicion, creacion y eliminacion de asignaturas completo

--- WebSkul/Controllers/SubjectController.cs
+++ WebSkul/Controllers/SubjectController.cs
@@ -9,17 +9,16 @@ namespace WebSkul.Controllers
     {
         private SchoolContext _context;
 
-        [Route("Subject/Index/")]
-        [Route("Subject/Index/{subjectId}")]
-
-        public IActionResult Index(string subjectId)
+        //[Route("Subject/Index/")]
+        //[Route("Subject/Index/{subjectId}")]
+        public IActionResult Index(string id)
         {
-            if (!string.IsNullOrWhiteSpace(subjectId))       //Si subjectId no es nulo
+            if (!string.IsNullOrWhiteSpace(id))       //Si subjectId no es nulo
             {
                 ViewBag.DynamicThing = "Hello world xd";
                 ViewBag.Date = DateTime.Now;
                 var subject = from sub in _context.Subjects
-                              where sub.Id == subjectId     //trae la asignatura que el id coincida con el id que pasemos
+                              where sub.Id == id     //trae la asignatura que el id coincida con el id que pasemos
                               select sub;
                 return View(subject.SingleOrDefault());  //Trae la primera o la asignatura por defecto de la db
             }
@@ -29,9 +28,62 @@ namespace WebSkul.Controllers
             }
         }
 
+        //Multi asignatura
         public IActionResult MultiSubject()
         {
             return View(_context.Subjects);      //Trae todas las asignaturas de la db
+        }
+
+        //Crear asignatura
+        public IActionResult Create()
+        {
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        [HttpPost]  //El metodo solo funcionara cuando la peticion sea por porst
+        public async Task<IActionResult> Create(Subject subject)
+        {
+            ViewBag.Date = DateTime.Now;
+
+            var course = _context.Courses.FirstOrDefault();
+            subject.CourseId = course.Id;
+            _context.Subjects.Add(subject);   //adiciona el courso q se pasa por parametro
+            _context.SaveChanges();     //Guarda los cambios
+            ViewBag.SubjectsCreated = "Alumno creado";
+            return View("index", subject);
+        }
+
+        //Editar asignatura
+        public IActionResult Update(string id)      //Editar curso
+        {
+            var subject = from sbj in _context.Subjects
+                          where sbj.Id == id
+                          select sbj;
+
+            return View("Create", subject.SingleOrDefault());
+        }
+        [HttpPost]
+        public IActionResult Update(Subject newData, string id)
+        {
+            var subjectSearch = from sbj in _context.Subjects
+                                where sbj.Id == id
+                                select sbj;
+            var subject = subjectSearch.SingleOrDefault();
+            subject.Name = newData.Name;
+            _context.SaveChanges();
+            return View("index", newData);
+        }
+
+        //Eliminar curso
+        public IActionResult Delete(string id)
+        {
+            var subjectSearch = from sbj in _context.Subjects
+                                where sbj.Id == id
+                                select sbj;
+            _context.Subjects.Remove(subjectSearch.FirstOrDefault());
+            _context.SaveChanges();
+            return RedirectToAction("index");
         }
 
         public SubjectController(SchoolContext context)
